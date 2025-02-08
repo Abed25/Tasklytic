a5663e61e143b2cf6d30003f46395d9c6419c1a8
Abed25 <officialwekesa254@gmail.com>
2025-02-08 11:53:38 +0300

    Home link

--- src/component/header.jsx
+++ src/component/header.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function Header(props) {
+  const navigate = useNavigate();
   const { name } = props;
   return (
     <div className="header">
-      <h2>Todo App</h2>
+      <h2 onClick={() => navigate("/")}>Todo App</h2>
     </div>
   );
 }
