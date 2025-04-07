3bc9e95bb39870b9b0a8ff7e2f0a23f70cc577d8
Abed25 <officialwekesa254@gmail.com>
2025-04-07 15:10:14 +0300

    Brand

--- src/component/footer.jsx
+++ src/component/footer.jsx
@@ -4,7 +4,10 @@ import "../styles/footer.css";
 function Footer() {
   return (
     <div className="footer">
-      <p>@copyrights 2025</p>
+      <p>
+        {" "}
+        &copy; {new Date().getFullYear()} superDev | Todo | All rights reserved.
+      </p>
     </div>
   );
 }
