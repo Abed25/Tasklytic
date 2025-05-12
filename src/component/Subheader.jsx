73d8c4d0df34bdd97ad0b2d01627d0ec3d73ca68
Abed25 <officialwekesa254@gmail.com>
2025-05-12 19:25:47 +0300

    Icons added to buttons

--- src/component/Subheader.jsx
+++ src/component/Subheader.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FaHome, FaListAlt, FaTasks } from "react-icons/fa";
 import "../styles/header.css";
 import "../styles/button.css";
 
@@ -7,32 +8,22 @@ export default function Subheader() {
   return (
     <div className="subheader">
       <Link to={"/home"}>
-        <button
-          className="normal"
-          style={{
-            backgroundColor: "#4C50AF",
-          }}
-        >
+        <button className="normal" style={{ backgroundColor: "#4C50AF" }}>
+          <FaHome style={{ marginRight: "8px" }} />
           Home
         </button>
       </Link>
+
       <Link to={"/list-of-tasks"}>
-        <button
-          className="normal"
-          style={{
-            backgroundColor: "#4CAF50",
-          }}
-        >
+        <button className="normal" style={{ backgroundColor: "#4CAF50" }}>
+          <FaListAlt style={{ marginRight: "8px" }} />
           View Tasks
         </button>
       </Link>
+
       <Link to={"/add-tasks"}>
-        <button
-          className="normal"
-          style={{
-            backgroundColor: "#4CAF50",
-          }}
-        >
+        <button className="normal" style={{ backgroundColor: "#4CAF50" }}>
+          <FaTasks style={{ marginRight: "8px" }} />
           Add Tasks
         </button>
       </Link>
